d5b59b675b85e40c4c90ae344374b5fb608be3b2
Daniel Isham <danielisham1@gmail.com>
2017-07-19 15:37:38 -0500

    prep for deployment

--- app/knexfile.js
+++ app/knexfile.js
@@ -3,14 +3,21 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_URL || 'reddit-clone',
+      database: process.env.DATABASE_URL || 'reddit-clone'
     }
   },
 
   test: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_URL || 'reddit-clone-test',
+      database: process.env.DATABASE_URL || 'reddit-clone-test'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || 'postgres://aplbrxwjcilsin:c7e4a9de03650cee5c2a4f9384e339f4e37b24dba45aee50753536264f11a576@ec2-54-243-252-232.compute-1.amazonaws.com:5432/dae7eolr4h1mvc'
     }
   }
 
