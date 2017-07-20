391a683d8c76238bc7a0e146126916a58f4c8924
Daniel Isham <danielisham1@gmail.com>
2017-07-20 09:47:22 -0500

    prep for deploy

--- app/knexfile.js
+++ app/knexfile.js
@@ -16,9 +16,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL || 'postgres://aplbrxwjcilsin:c7e4a9de03650cee5c2a4f9384e339f4e37b24dba45aee50753536264f11a576@ec2-54-243-252-232.compute-1.amazonaws.com:5432/dae7eolr4h1mvc'
-    }
+    connection: `${process.env.DATABASE_URL}?ssl=true`
   }
 
 };
