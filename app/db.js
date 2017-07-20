6ed16a9f875cbfdd816b5c4b02dd91e7d1b9a160
Daniel Isham <danielisham1@gmail.com>
2017-07-20 09:53:16 -0500

    prep for deploy

--- app/db.js
+++ app/db.js
@@ -1,1 +1,3 @@
-module.exports = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'development'])
+module.exports = require('knex')(require('./knexfile')[process.env.NODE_ENV || 'development']);
+console.log(process.env.NODE_ENV);
+console.log(process.env.DATABASE_URL);
